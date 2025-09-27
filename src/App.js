12bcf324550f39f5f0ae1ef27865fb4d42053303
MohammadReza Alizadeh <alizadehmohammadreza1384@gmail.com>
2025-09-27 18:23:19 +0330

    .

--- src/App.js
+++ src/App.js
@@ -9,29 +9,26 @@ import AboutUs from "./pages/AboutUs";
 import ContactUs from "./pages/ContactUs";
 import Footer from "./components/Footer";
 import ScrollToTop from "./components/ScrollToTop";
-import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <div className="min-h-screen">
-        <header className="w-full">
-          <NavBar />
-          <SearchBar />
-        </header>
-        <ScrollToTop />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/article/:id" element={<ArticlePage />} />
-          <Route path="/articles" element={<ArticlesPage />} />
-          <Route path="/aboutus" element={<AboutUs />} />
-          <Route path="/contactus" element={<ContactUs />} />
-        </Routes>
-        <footer>
-          <Footer />
-        </footer>
-      </div>
-    </BrowserRouter>
+    <div className="min-h-screen">
+      <header className="w-full">
+        <NavBar />
+        <SearchBar />
+      </header>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/SabtJavid/" element={<Home />} />
+        <Route path="/article/:id" element={<ArticlePage />} />
+        <Route path="/articles" element={<ArticlesPage />} />
+        <Route path="/aboutus" element={<AboutUs />} />
+        <Route path="/contactus" element={<ContactUs />} />
+      </Routes>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import AppProviders from "./context/AppProviders";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/">
       <AppProviders>
         <App />
       </AppProviders>
